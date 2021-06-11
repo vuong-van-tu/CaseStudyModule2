71d1f780cd658af5cd477c920ba0d92787f35722
vuongvantu <vuongvantu2001x@gmail.com>
2021-06-11 17:55:54 +0700

    find by name thành công

--- src/sample/Controller/ControllerCrud.java
+++ src/sample/Controller/ControllerCrud.java
@@ -53,14 +53,37 @@ public class ControllerCrud implements Initializable {
     @FXML
     private TableColumn<Custom,String> cmnd;
     @FXML
+    private TableView<Custom> tableCus1;
+    @FXML
+    private TableColumn<Custom,String> name1;
+    @FXML
+    private TableColumn<Custom,Integer> age1;
+    @FXML
+    private TableColumn<Custom, String> address1;
+    @FXML
+    private TableColumn<Custom, String> gender1;
+    @FXML
+    private TableColumn<Custom, String> numberPhone1;
+    @FXML
+    private TableColumn<Custom,String> cmnd1;
+    @FXML
     private Button delete;
     @FXML
     private TextField ipdelete;
     @FXML
-            private TextField ipedit;
+    private TextField ipedit;
+    @FXML
+    private TextField ipfind;
 
 
     ObservableList<Custom> listCus = FXCollections.observableArrayList();
+
+    public ControllerCrud() {
+        listCus.add(new Custom("Tú",20,"Hà Nội","Nam","0338376563","033201005564"));
+        listCus.add(new Custom("Tú",27,"Hưng Yên","Nam","0334319874","033225122001"));
+        listCus.add(new Custom("Chiến",25,"Hà Nội 2","Nam","0345989349","109309435783"));
+        listCus.add(new Custom("Chương",29,"Nam Định","Nam","0338390843","913940928329"));
+    }
 
     public void add(ActionEvent event){
         listCus.add(new Custom(ipname.getText(),Integer.parseInt(ipage.getText()),ipaddress.getText(), (String) ipgender.getValue(),ipnumberphone.getText(),ipcmnd.getText()));
@@ -76,6 +99,25 @@ public class ControllerCrud implements Initializable {
     }
     public void delete(ActionEvent event){
         listCus.removeIf(custom -> ipdelete.getText().equals(custom.getCmnd()));
+    }
+    public ObservableList<Custom> findByName(){
+        ObservableList<Custom> listName = FXCollections.observableArrayList();
+        for (Custom cus:listCus) {
+            if (ipfind.getText().equals(cus.getName())){
+                listName.add(cus);
+            }
+        }
+        return listName;
+    }
+    public void find(){
+        name1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        age1.setCellValueFactory(new PropertyValueFactory<>("age"));
+        address1.setCellValueFactory(new PropertyValueFactory<>("address"));
+        gender1.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        numberPhone1.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        cmnd1.setCellValueFactory(new PropertyValueFactory<>("cmnd"));
+        tableCus1.setItems(findByName());
+        ipfind.clear();
     }
 
     @Override
